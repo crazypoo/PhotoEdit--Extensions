06633933c29752bca2d91a9c6f7dabad9b286517
crazypoo <273277355@qq.com>
2014-06-12 16:24:06 +0800

    Initial Commit

---
+++ Photo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photo
+//
+//  Created by crazypoo on 14/6/12.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
